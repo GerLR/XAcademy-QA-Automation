1ca26c72e5d789d9d4e0883401dab1a9403bbf7f
Gerardo Romero <gerardolauroromero@gmail.com>
2026-06-02 00:17:11 -0300

    test(support): crear custom command para simplificar el login

---
+++ cypress/support/commands.js
@@ -0,0 +1,9 @@
+Cypress.Commands.add("login", (username, password) => {
+  cy.visit("https://www.saucedemo.com/");
+
+  cy.get('[data-test="username"]').should("be.visible").type(username);
+
+  cy.get('[data-test="password"]').should("be.visible").type(password);
+
+  cy.get('[data-test="login-button"]').click();
+});
